e084b6287cb6c6dd313e3b44e4602df0cf902528
vinayak <vinayaks0031@gmail.com>
2021-05-20 14:11:24 +0530

    working stage

---
+++ app.js
@@ -0,0 +1,36 @@
+
+function getNews(name) {
+
+
+    fetch(`https://bing-news-search1.p.rapidapi.com/news/search?q=${name}&safeSearch=Off&textFormat=Raw&freshness=Day`, {
+        "method": "GET",
+        "headers": {
+            "x-bingapis-sdk": "true",
+            "x-rapidapi-key": "d650ee175emsh8df1625b681e66ap1d8743jsnb317017171cc",
+            "x-rapidapi-host": "bing-news-search1.p.rapidapi.com"
+        }
+    })
+        .then(response => response.json()).then((data) => {
+            let accordion = document.getElementById("accordion");
+            let html = "";
+            console.log(data);
+            let i = 0;
+            for (const key in data.value) {
+                html += ` <div class="accordion-item bg-dark my-3">
+                <h2 class="accordion-header" id="heading${key}">
+                    <button class="accordion-button collapsed text-white bg-dark" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapse${key}" aria-expanded="true" aria-controls="collapse${key}">
+                        <b>Breaking News ${++i} </b>:${data.value[key].name}
+                    </button>
+                </h2>
+                <div id="collapse${key}" class="accordion-collapse collapse " aria-labelledby="heading${key}"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body text-white">
+                        ${data.value[key].description} . <a href="${data.value[key].url}" class="btn btn-link px-1 py-1" target="_blank">Read full News Here</a>
+                    </div>
+                </div>
+            </div>`
+            }
+            accordion.innerHTML = html;
+        })
+}
